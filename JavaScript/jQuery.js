f93f157a5dad146e53574461ae27fcf90e338edb
Ioan Georgiev <ioan.georgiev62@gmail.com>
2016-04-23 16:58:36 +0300

    This is my examples

---
+++ JavaScript/jQuery.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $(".scrollup").fadeIn(400);
+        } else {
+            $(".scrollup").fadeOut(400);
+        }
+    });
+
+    $(".scrollup").click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 700);
+        return false;
+    });
+});
